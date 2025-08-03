f0e69689faa00d1fd5f66b0162ecf319608fe8f2
Sameer Jain <sameer.jain@synlabs.io>
2025-08-03 14:28:13 +0530

    Refactor code structure for improved readability and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MainLayout from "./components/layout/main-layout";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <MainLayout />
-  </StrictMode>
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<MainLayout />
+		</QueryClientProvider>
+	</StrictMode>
 );
