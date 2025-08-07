1dfedd246d01627cbcf2794866530f440fa9cf8a
Sameer Jain <sameer.jain@synlabs.io>
2025-08-07 07:26:21 +0530

    added axios setup

---
+++ src/api/axios.ts
@@ -0,0 +1,55 @@
+import type { AxiosResponse } from "axios";
+import axios, { AxiosError } from "axios";
+
+const instance = axios.create({
+	baseURL: "/",
+	timeout: 30000,
+});
+
+const normalizeResponse = (response: AxiosResponse) => {
+	return Promise.resolve({
+		message:
+			response?.data?.message ||
+			(response.status === 200
+				? "Request successful"
+				: "Unable to process request. Try again later!"),
+		data: response.data?.data || response.data || null,
+		status: response.status,
+		headers: response.headers,
+		fullResponse: response.data,
+	});
+};
+
+const normalizeError = (error: AxiosError<any>) => {
+	return Promise.reject({
+		message: error.response?.data?.message || null,
+		data: error.response?.data?.data || error.response?.data || null,
+		status: error.status,
+		responseStatus: error.response?.data?.error_code || error.status,
+		error: error,
+		fullResponse: error.response,
+	});
+};
+
+export const request = {
+	get: (url: string, headers?: any) =>
+		instance
+			.get(url, { ...headers })
+			.then(res => normalizeResponse(res))
+			.catch((err: AxiosError) => normalizeError(err)),
+	post: <B>(url: string, body: B, headers?: any) =>
+		instance
+			.post(url, body, { ...headers })
+			.then(res => normalizeResponse(res))
+			.catch((err: AxiosError) => normalizeError(err)),
+	put: <B>(url: string, body: B, headers?: any) =>
+		instance
+			.put(url, body, { ...headers })
+			.then(res => normalizeResponse(res))
+			.catch((err: AxiosError) => normalizeError(err)),
+	delete: (url: string) =>
+		instance
+			.delete(url)
+			.then(res => normalizeResponse(res))
+			.catch((err: AxiosError) => normalizeError(err)),
+};
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	server: {
+		allowedHosts: true,
+		proxy: {
+			"/api": {
+				target: "http://localhost:8080/" + "/api",
+				changeOrigin: true,
+				rewrite: path => path.replace(/^\/api/, ""),
+				ws: true,
+				secure: false,
+			},
+		},
+	},
 });
